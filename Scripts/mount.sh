404bbab1f0dd72f15622463d09859d0085a078c8
CHEF-KOCH <CHEF-KOCH@protonmail.com>
2020-06-18 10:37:52 +0200

    Scripts section added
    Change according to your drive names.

---
+++ Scripts/mount.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+/usr/bin/rclone mount Mega: /home/alistair/OnlineDrives/Mega &
+/usr/bin/rclone mount Onedrive: /home/alistair/OnlineDrives/Onedrive &
+/usr/bin/rclone mount GDrive_AG: /home/alistair/OnlineDrives/GDrive_AG &
+/usr/bin/rclone mount GDrive_KKCC: /home/alistair/OnlineDrives/GDrive_KKCC &
